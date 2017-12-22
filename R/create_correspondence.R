d9234b3957e87b62598a3166998da46bbc9cddac
agricolamz <agricolamz@gmail.com>
2017-12-22 15:28:28 +0300

    add first function

---
+++ R/create_correspondence.R
@@ -0,0 +1,33 @@
+#' Create IPA-transcription correspondence
+#'
+#' This function returns a dataframe with IPA id, IPA symbol and transcription. If user doesn't provide any data, the function returns a template that is used in lingphonology, so users can save it to the disc and open it with appropriate software.
+#' @param correspondence a character vector, containing symbol correspondences to IPA symbols in orthographical system of user's files
+#' @author George Moroz <agricolamz@gmail.com>
+#' @examples
+#' create_correspondence()
+#' @export
+
+create_correspondence <- function(correspondence = "") {
+  # change correspondence to string -----------------------------------------
+  correspondence <- as.character(correspondence)
+
+  # list of IPA symbols; new symbols should be added on the end! --------------
+  ipa <- c("i", "y", "ɨ", "ʉ", "ɯ", "u", "ɪ", "ʏ", "ʊ",
+           "ɪ̈",
+           "ʊ̈",
+           "e")
+
+  # create a dataframe with IPA (and/for user's values) ---------------------
+  correspondence_df <- data.frame(
+    ipa,
+    transcription = correspondence,
+    stringsAsFactors = FALSE
+  )
+
+  # return whole list or remove unused IPA symbols --------------------------
+  if (length(correspondence) == 1) {
+    correspondence_df
+  } else{
+    correspondence_df[correspondence_df$transcription != "", ]
+  }
+}
